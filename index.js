fe9aebb31eee0a39dd85aed4a9dd4bcc4f36a2a5
Victor <vx_zz@hotmail.com>
2022-05-29 10:01:29 -0400

    commit tree be8cdeca34a6b51d2d3446d138acc8c8d464715a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail,
+        })
+    })
+    .then (res => res.json())
+    .then (object => {
+        document.querySelector("body").append(object.id)
+    })
+    .catch(error => {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+        document.querySelector("body").append(error.message)
+      });
+
+
+}
+
+submitData("victor", "vic_42@gmail.com")
